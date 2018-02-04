00f376b10afebbdd10b4175803582da1e8bb79e6
zjfsharp <zjfsharp@sina.cn>
2018-02-04 22:57:24 +0800

    1. add file Latte, Configurator, ConfigType in module latte-core to configure project 2. init configurations in MainActivity

--- example/src/main/java/com/zjfsharp/festec/example/MainActivity.java
+++ example/src/main/java/com/zjfsharp/festec/example/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.zjfsharp.latte.app.Latte;
+
+//全局只持有单个Activity
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Latte.init(this).withApiHost("http://127.0.0.1/").configure(); //简易的标准初始化模式，规范
+
     }
 }
---
+++ latte-core/src/main/java/com/zjfsharp/latte/app/ConfigType.java
@@ -0,0 +1,12 @@
+package com.zjfsharp.latte.app;
+
+/**
+ * Created by zjfsharp on 2018/2/4.
+ */
+
+public enum ConfigType{
+    API_HOST,
+    APPLICATION_CONTEXT,
+    CONFIG_READY,
+    ICON
+}
